a26464cf3fe5978b3aba555b6d6374300d78c91b
grewaldpsg <97416285+grewaldpsg@users.noreply.github.com>
2022-07-15 08:09:47 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import './App.css';
 
 const App = () => {
   const [courseGoals, setCourseGoals] = useState([
-    { text: 'Do all exercises!', id: 'g1' },
-    { text: 'Finish the course!', id: 'g2' },
+    { text: 'Learn React!', id: 'g1' },
+    { text: 'Learn Node!', id: 'g2' },
   ]);
 
   const addGoalHandler = (enteredText) => {
